e2d598b1a7a23bd5934f274228e4fffbbccd787a
Dr. Leo <fhaxbox66@gmail.com>
2015-10-25 22:52:45 +0100

    refactorings

--- pandasdmx/reader/sdmxml.py
+++ pandasdmx/reader/sdmxml.py
@@ -52,7 +52,7 @@ class SDMXMLReader(BaseReader):
 
     _root_tag = XPath('name(//*[1])')
 
-    _str2path = {
+    _paths = {
         'footer_text': 'com:Text/text()',
         'dataflow_from_msg': 'mes:Structures/str:Dataflows',
         'constraint_attachment': 'str:ConstraintAttachment',
@@ -84,9 +84,6 @@ class SDMXMLReader(BaseReader):
         'obs_value_path': 'gen:ObsValue/@value',
         'attr_id_path': 'gen:Attributes/gen:Value/@id',
         'attr_values_path': 'gen:Attributes/gen:Value/@value',
-    }
-
-    _cls2path = {
         model.Code: 'str:Code',
         model.Categorisation: 'str:Categorisation',
         model.CategoryScheme: 'mes:Structures/str:CategorySchemes/str:CategoryScheme',
@@ -117,10 +114,9 @@ class SDMXMLReader(BaseReader):
         model.GenericDataSet: 'mes:DataSet',
     }
 
-    for d in (_cls2path, _str2path):
-        for key, path in d.items():
-            d[key] = XPath(path, namespaces=_nsmap)
-    del d, key, path
+    for key, path in _paths.items():
+        _paths[key] = XPath(path, namespaces=_nsmap)
+    del key, path
 
     def read_identifiables(self, cls,  sdmxobj, offset=None):
         '''
@@ -129,10 +125,10 @@ class SDMXMLReader(BaseReader):
         i.e. have an 'id' attribute. This will be used as dict keys.
         If sdmxobj does not inherit from dict: return a new DictLike.
         '''
-        path = self._cls2path[cls]
+        path = self._paths[cls]
         if offset:
             try:
-                base = self._str2path[offset](sdmxobj._elem)[0]
+                base = self._paths[offset](sdmxobj._elem)[0]
             except IndexError:
                 return None
         else:
@@ -145,7 +141,7 @@ class SDMXMLReader(BaseReader):
 
     def read_instance(self, cls, sdmxobj, offset=None, first_only=True):
         '''
-        If cls in _cls2path and matches,
+        If cls in _paths and matches,
         return an instance of cls with the first XML element,
         or, if fest_only is False, a list of cls instances 
         for all elements found,
@@ -153,12 +149,12 @@ class SDMXMLReader(BaseReader):
         '''
         if offset:
             try:
-                base = self._str2path[offset](sdmxobj._elem)[0]
+                base = self._paths[offset](sdmxobj._elem)[0]
             except IndexError:
                 return None
         else:
             base = sdmxobj._elem
-        result = self._cls2path[cls](base)
+        result = self._paths[cls](base)
         if result:
             if first_only:
                 return cls(self, result[0])
@@ -167,26 +163,26 @@ class SDMXMLReader(BaseReader):
 
     def read_subclass_instance(self, target_cls, sdmxobj, offset=None, first_only=True):
         '''
-        Iterate over model classes in _cls2path which are subclasses of
+        Iterate over model classes in _paths which are subclasses of
         'target_cls' and instanciate the classes whose xpath expression returns a non-empty result.
         Return a list of subclass instances.
         '''
         if offset:
-            base = self._str2path[offset](sdmxobj._elem)[0]
+            base = self._paths[offset](sdmxobj._elem)[0]
         else:
             base = sdmxobj._elem
-        subclasses = (c for c in self._cls2path if type(c) is type
+        subclasses = (c for c in self._paths if type(c) is type
                       and issubclass(c, target_cls))
         matches = []
         for cls in subclasses:
-            match = self._cls2path[cls](base)
+            match = self._paths[cls](base)
             if match:
                 for m in match:
                     matches.append(cls(self, m))
         return matches
 
     def read_as_str(self, name, sdmxobj, first_only=True):
-        result = self._str2path[name](sdmxobj._elem)
+        result = self._paths[name](sdmxobj._elem)
         if result:
             if first_only:
                 return result[0]
@@ -266,55 +262,55 @@ class SDMXMLReader(BaseReader):
         'SeriesObservation', ('dim', 'value', 'attrib'))
 
     def iter_generic_obs(self, sdmxobj, with_value, with_attributes):
-        for obs in self._str2path['generic_obs_path'](sdmxobj._elem):
+        for obs in self._paths['generic_obs_path'](sdmxobj._elem):
             # Construct the namedtuple for the ObsKey.
             # The namedtuple class is created on first iteration.
-            obs_key_values = self._str2path['obs_key_values_path'](obs)
+            obs_key_values = self._paths['obs_key_values_path'](obs)
             try:
                 obs_key = ObsKeyTuple._make(obs_key_values)
             except NameError:
-                obs_key_id = self._str2path['obs_key_id_path'](obs)
+                obs_key_id = self._paths['obs_key_id_path'](obs)
                 ObsKeyTuple = namedtuple_factory('ObsKey', obs_key_id)
                 obs_key = ObsKeyTuple._make(obs_key_values)
             if with_value:
-                obs_value = self._str2path['obs_value_path'](obs)[0]
+                obs_value = self._paths['obs_value_path'](obs)[0]
             else:
                 obs_value = None
             if with_attributes:
-                obs_attr_values = self._str2path['attr_values_path'](obs)
-                obs_attr_id = self._str2path['attr_id_path'](obs)
+                obs_attr_values = self._paths['attr_values_path'](obs)
+                obs_attr_id = self._paths['attr_id_path'](obs)
                 obs_attr = DictLike(zip(obs_attr_id, obs_attr_values))
             else:
                 obs_attr = None
             yield self._ObsTuple(obs_key, obs_value, obs_attr)
 
     def generic_series(self, sdmxobj):
-        path = self._cls2path[model.Series]
+        path = self._paths[model.Series]
         for series in path(sdmxobj._elem):
             yield model.Series(self, series, dataset=sdmxobj)
 
     def generic_groups(self, sdmxobj):
-        path = self._cls2path[model.Group]
+        path = self._paths[model.Group]
         for series in path(sdmxobj._elem):
             yield model.Group(self, series)
 
     def series_key(self, sdmxobj):
-        series_key_id = self._str2path['series_key_id_path'](sdmxobj._elem)
-        series_key_values = self._str2path[
+        series_key_id = self._paths['series_key_id_path'](sdmxobj._elem)
+        series_key_values = self._paths[
             'series_key_values_path'](sdmxobj._elem)
         SeriesKeyTuple = namedtuple_factory('SeriesKey', series_key_id)
         return SeriesKeyTuple._make(series_key_values)
 
     def group_key(self, sdmxobj):
-        group_key_id = self._str2path['group_key_id_path'](sdmxobj._elem)
-        group_key_values = self._str2path[
+        group_key_id = self._paths['group_key_id_path'](sdmxobj._elem)
+        group_key_values = self._paths[
             'group_key_values_path'](sdmxobj._elem)
         GroupKeyTuple = namedtuple_factory('GroupKey', group_key_id)
         return GroupKeyTuple._make(group_key_values)
 
     def series_attrib(self, sdmxobj):
-        attr_id = self._str2path['attr_id_path'](sdmxobj._elem)
-        attr_values = self._str2path['attr_values_path'](sdmxobj._elem)
+        attr_id = self._paths['attr_id_path'](sdmxobj._elem)
+        attr_values = self._paths['attr_values_path'](sdmxobj._elem)
         return DictLike(zip(attr_id, attr_values))
 
     def iter_generic_series_obs(self, sdmxobj, with_value, with_attributes,
@@ -322,14 +318,14 @@ class SDMXMLReader(BaseReader):
         for obs in sdmxobj._elem.iterchildren(
                 '{http://www.sdmx.org/resources/sdmxml/schemas/v2_1/data/generic}Obs',
                 reversed=reverse_obs):
-            obs_dim = self._str2path['generic_series_dim_path'](obs)[0]
+            obs_dim = self._paths['generic_series_dim_path'](obs)[0]
             if with_value:
-                obs_value = self._str2path['obs_value_path'](obs)[0]
+                obs_value = self._paths['obs_value_path'](obs)[0]
             else:
                 obs_value = None
             if with_attributes:
-                obs_attr_values = self._str2path['attr_values_path'](obs)
-                obs_attr_id = self._str2path['attr_id_path'](obs)
+                obs_attr_values = self._paths['attr_values_path'](obs)
+                obs_attr_id = self._paths['attr_id_path'](obs)
                 obs_attr = DictLike(zip(obs_attr_id, obs_attr_values))
             else:
                 obs_attr = None

ef33099908c4d7508efbde35fdc4dc08dc326b59
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2018-11-06 15:56:06 +0100

    add test 'read_xml_structure_insee'
    - Check that structure references are correct in this example file

--- tests/test_reader_xml.py
+++ tests/test_reader_xml.py
@@ -35,3 +35,15 @@ def test_read_xml(req, path):
 @pytest.mark.parametrize('path', test_structure)
 def test_read_xml_structure(req, path):
     req.get(fromfile=path).msg
+
+
+def test_read_xml_structure_insee(req):
+    msg = req.get(fromfile=test_structure[4]).msg
+
+    # Same objects referenced
+    assert (id(msg.dataflow['IPI-2010-A21'].structure) ==
+            id(msg.structure['IPI-2010-A21']))
+
+    # Number of dimensions loaded correctly
+    dsd = msg.structure['IPI-2010-A21']
+    assert len(dsd.dimensions) == 4

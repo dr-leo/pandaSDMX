bbe0e2fed1d31b57c6a87cd8b03ef65a02b74767
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2018-11-11 14:09:33 +0100

    rewrite/streamline test_dsd.py using fixtures

--- tests/test_dsd.py
+++ tests/test_dsd.py
@@ -5,63 +5,52 @@
 
 @author: Dr. Leo
 '''
-import unittest
-import pytest
+import pandasdmx
+from pandasdmx import model
 
-from pandasdmx import model, Request
-
-from . import test_data_path
+from . import MessageTest, test_data_path
 
 
-class Test_ESTAT_dsd_apro_mk_cola(unittest.TestCase):
+class Test_ESTAT_dsd_apro_mk_cola(MessageTest):
+    path = test_data_path / 'estat'
+    filename = 'apro_dsd.xml'
 
-    def setUp(self):
-        self.estat = Request('ESTAT')
-        filepath = test_data_path / 'estat' / 'apro_dsd.xml'
-        self.resp = self.estat.get(fromfile=filepath)
+    def test_codelists_keys(self, msg):
+        assert len(msg.codelist) == 6
+        assert isinstance(msg.codelist.CL_GEO, model.Codelist)
 
-    def test_codelists_keys(self):
-        self.assertEqual(len(self.resp.msg.codelist), 6)
-        self.assertIsInstance(self.resp.msg.codelist.CL_GEO,
-                              model.Codelist)
+    def test_codelist_name(self, msg):
+        assert msg.codelist.CL_GEO.UK.name.en == 'United Kingdom'
+        assert msg.codelist.CL_FREQ.name.en == 'FREQ'
 
-    def test_codelist_name(self):
-        self.assertEqual(
-            self.resp.msg.codelist.CL_GEO.UK.name.en, 'United Kingdom')
-        assert self.resp.codelist.CL_FREQ.name.en == 'FREQ'
+    def test_code_cls(self, msg):
+        assert isinstance(msg.codelist.CL_FREQ.D, model.Code)
 
-        def test_code_cls(self):
-            self.assertIsInstance(
-                self.resp.codelist.CL_FREQ.D, model.Code)
-
-    def test_writer(self):
-        df = self.resp.write(rows='codelist')
-        self.assertEqual(df.shape, (79, 2))
+    def test_writer(self, msg):
+        df = pandasdmx.to_pandas(msg.codelist)
+        # TODO test the actual expected content
+        assert df.shape == (79, 2)
 
 
-class test_dsd_common(unittest.TestCase):
+class test_dsd_common(MessageTest):
+    path = test_data_path / 'common'
+    filename = 'common.xml'
 
-    def setUp(self):
-        self.estat = Request('ESTAT')
-        filepath = test_data_path / 'common' / 'common.xml'
-        self.resp = self.estat.get(fromfile=filepath)
+    def test_codelists_keys(self, msg):
+        assert len(msg.codelist) == 5
+        assert isinstance(msg.codelist.CL_FREQ, model.Codelist)
 
-    def test_codelists_keys(self):
-        self.assertEqual(len(self.resp.msg.codelist), 5)
-        self.assertIsInstance(self.resp.msg.codelist.CL_FREQ,
-                              model.Codelist)
+    def test_codelist_name(self, msg):
+        assert msg.codelist.CL_FREQ.D.name.en == 'Daily'
 
-    def test_codelist_name(self):
-        self.assertEqual(self.resp.msg.codelist.CL_FREQ.D.name.en, 'Daily')
+    def test_code_cls(self, msg):
+        assert isinstance(msg.codelist.CL_FREQ.D, model.Code)
 
-    def test_code_cls(self):
-        self.assertIsInstance(self.resp.msg.codelist.CL_FREQ.D, model.Code)
-
-    def test_annotations(self):
+    def test_annotations(self, msg):
         code = self.resp.codelist.CL_FREQ.A
         anno_list = list(code.annotations)
-        self.assertEqual(len(anno_list), 1)
+        assert len(anno_list) == 1
         a = anno_list[0]
-        self.assertIsInstance(a, model.Annotation)
+        assert isinstance(a, model.Annotation)
         assert a.text.en.startswith('It is')
-        self.assertEqual(a.type, 'NOTE')
+        assert a.type == 'NOTE'

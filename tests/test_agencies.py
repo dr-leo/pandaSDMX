71ff73b95ee29c46acab7cf094684c8fe1c5eec0
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2018-11-11 16:00:49 +0100

    merge agency tests to a single file, add ESTAT and OECD

--- tests/test_agencies.py
+++ tests/test_agencies.py
@@ -23,3 +23,21 @@ class AgencyTest:
     @pytest.mark.parametrize('endpoint', structure_endpoints)
     def test_common_structure_endpoints(self, req, endpoint):
         req.get(endpoint)
+
+
+class TestESTAT(AgencyTest):
+    agency_id = 'ESTAT'
+
+
+class TestUNSD(AgencyTest):
+    # FIXME
+    # - http://data.un.org/WS/rest/datastructure/UNSD
+    #   /latest?references=parentsandsiblings : raises ParseError: <Concept:
+    #  'FREQ'=''> not located in <ConceptScheme: 'MDG_DATA_CONCEPTS', 0 items>
+    agency_id = 'UNSD'
+
+
+@pytest.mark.xfail(reason='400 errors; JSON endpoints not correctly handled in'
+                          ' api.Request')
+class TestOECD(AgencyTest):
+    agency_id = 'OECD'

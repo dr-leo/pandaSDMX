9a925b82b51e5cbd4f5f43b1dc819d454546b58f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2018-11-11 16:00:49 +0100

    merge agency tests to a single file, add ESTAT and OECD

--- tests/agencies/test_unsd.py
+++
@@ -1,9 +0,0 @@
-# FIXME
-# - test_common_endpoints(endpoint='codelist') has an XML tag
-#  'AnnotationTitle' not currently handled by reader.sdmxml
-
-from . import AgencyTest
-
-
-class TestUNSD(AgencyTest):
-    agency_id = 'UNSD'

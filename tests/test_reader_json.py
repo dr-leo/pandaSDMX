9cc6cfe072726c68f9eb2ebd641ec6218fbc2b3b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2018-11-16 15:08:30 +0100

    remove JSON write_source() test
    - This was equivalent to copying a file, which can be done by other
      means.

--- tests/test_reader_json.py
+++ tests/test_reader_json.py
@@ -1,10 +1,5 @@
-import json
-import os
-
-import pytest
-
 import pandasdmx
-from pandasdmx.api import Request
+import pytest
 
 from . import test_data_path, test_files
 
@@ -18,26 +13,3 @@ def test_json_read(path):
 def test_header():
     resp = pandasdmx.open_file(test_data_path / 'json' / 'exr-flat.json')
     assert resp.header.id == '62b5f19d-f1c9-495d-8446-a3661ed24753'
-
-
-# TODO store the source in the Response
-@pytest.mark.skip('DISABLED temporarily: relies on removed Message._reader')
-def test_write_source(self):
-    """Test the write_source() method."""
-    req = Request()
-    for name in sample_data.keys():  # Should use test_files
-        orig_fn = self._filepath(name)
-        temp_fn = self._filepath(name + '-write-source')
-
-        # Read the message
-        resp = req.get(fromfile=orig_fn)
-
-        # Write to a temporary JSON file
-        resp.write_source(temp_fn)
-
-        # Read the two files and compare JSON (ignores ordering)
-        with open(orig_fn) as orig, open(temp_fn) as temp:
-            assert json.load(orig) == json.load(temp)
-
-        # Delete the temporary file
-        os.remove(temp_fn)

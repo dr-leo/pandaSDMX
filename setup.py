59e77bbf00b057293c5f97ff951375eafe016a2c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-05-16 11:19:45 +0200

    Simplify Travis config

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ from codecs import open
 # Publish README on PYPI when uploading.
 long_descr = open('description.rst', 'r', encoding='utf8').read()
 
+TESTS_REQUIRE = [
+    'odo>=0.5',
+    'pytest>=3.3',
+    'pytest-remotedata>=0.3.1',
+    'requests-mock>=1.4',
+    'requests>=2.7',
+    ]
+
 setup(name='pandaSDMX',
       version='1.0.0-dev',
       description='A client for SDMX - Statistical Data and Metadata eXchange',
@@ -15,17 +23,18 @@ setup(name='pandaSDMX',
       package_data={'pandasdmx': ['sources.json']},
       url='https://github.com/dr-leo/pandasdmx',
       install_requires=[
-          'pandas',
-          'lxml',
-          'pydantic',
+          'pandas>=0.20',
+          'lxml>=3.6',
+          'pydantic>=0.25',
           'requests',
-          'jsonpath-rw',
-          'setuptools',
+          'setuptools>19',
           ],
       extras_require={
         'cache': ['requests_cache'],
+        'docs': ['sphinx>=1.5'],
+        'tests': TESTS_REQUIRE,
         },
-      tests_require=['odo', 'pytest-remotedata', 'requests-mock'],
+      tests_require=TESTS_REQUIRE,
       keywords='statistics SDMX pandas data economics science',
       zip_safe=True,
       provides=['pandasdmx'],

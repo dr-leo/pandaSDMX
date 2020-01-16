cd87d7b4658bc055b3d9bba5e4c2ea3dbe057923
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-01-16 11:53:50 +0100

    Add gregorianYearMonth to FacetValueType
    - Appears recently in data from Norges Bank. See comment.

--- pandasdmx/model.py
+++ pandasdmx/model.py
@@ -258,18 +258,18 @@ UsageStatus = Enum('UsageStatus', 'mandatory conditional')
 # NB three diagrams in the spec show this enumeration containing
 #    'gregorianYearMonth' but not 'gregorianYear' or 'gregorianMonth'. The
 #    table in §3.6.3.3 Representation Constructs does the opposite. One ESTAT
-#    query (via SGR) shows a real-world usage of 'gregorianYear', so the table
-#    is followed.
+#    query (via SGR) shows a real-world usage of 'gregorianYear'; while one NB
+#    query shows usage of 'gregorianYearMonth'; so all three are included.
 FacetValueType = Enum(
     'FacetValueType',
     """string bigInteger integer long short decimal float double boolean uri
     count inclusiveValueRange alpha alphaNumeric numeric exclusiveValueRange
     incremental observationalTimePeriod standardTimePeriod basicTimePeriod
-    gregorianTimePeriod gregorianYear gregorianMonth gregorianDay
-    reportingTimePeriod reportingYear reportingSemester reportingTrimester
-    reportingQuarter reportingMonth reportingWeek reportingDay dateTime
-    timesRange month monthDay day time duration keyValues identifiableReference
-    dataSetReference""")
+    gregorianTimePeriod gregorianYear gregorianMonth gregorianYearMonth
+    gregorianDay reportingTimePeriod reportingYear reportingSemester
+    reportingTrimester reportingQuarter reportingMonth reportingWeek
+    reportingDay dateTime timesRange month monthDay day time duration keyValues
+    identifiableReference dataSetReference""")
 
 ConstraintRoleType = Enum('ConstraintRoleType', 'allowable actual')
 

09e3f2abcda39d19f5101adf73d6550ece2eb031
Allen <64019758+aboddie@users.noreply.github.com>
2022-03-23 09:36:21 -0400

    Fix error message
    Update error message to use the variable in the message instead of printing unformatted message then the variable.

--- pandasdmx/source/__init__.py
+++ pandasdmx/source/__init__.py
@@ -183,7 +183,7 @@ def add_source(info, id=None, override=False, **kwargs):
     info.update(kwargs)
 
     if id in sources and not override:
-        raise ValueError("Data source '%s' already defined; use override=True", id)
+        raise ValueError("Data source '%s' already defined; use override=True" % id)
 
     # Maybe import a subclass that defines a hook
     SourceClass = Source
